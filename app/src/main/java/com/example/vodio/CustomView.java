784555a289cdabf7b5440a5bdade816aaec8674d
xiongwei <15111467924@163.com>
2020-03-23 08:36:49 +0800

    家里的代码没有提交和更新 这是什么问题呢

--- app/src/main/java/com/example/vodio/CustomView.java
+++ app/src/main/java/com/example/vodio/CustomView.java
@@ -5,23 +5,34 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
+
+import androidx.annotation.Nullable;
 
 public class CustomView extends View {
     String pathName = "/mnt/sdcard/DCIM/Camera/IMG_20200110_104110.jpg";
     Paint paint = new Paint();
     Bitmap bitmap;
+
     public CustomView(Context context) {
         super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
         bitmap = BitmapFactory.decodeFile(pathName);  // 获取bitmap
     }
 
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        // 不建议在onDraw做任何分配内存的操作
         if (bitmap != null) {
             canvas.drawBitmap(bitmap, 0, 0, paint);
         }
